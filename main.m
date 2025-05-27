a912da262b52b3bf6aa59b677f3ff5ada54dd488
OrigamiShido <113040308+OrigamiShido@users.noreply.github.com>
2025-05-27 18:03:35 +0800

    0.0.1

--- main.m
+++ main.m
@@ -1,8 +1,29 @@
 % tle_struct=TLE_read([pwd,'\gp.tle']);
 
+latitude=30.5288888;
+longitude=114.3530555;
+altitude=56;
+starttime=datetime(2024,1,1,0,0,0,'TimeZone','UTC');
+stoptime=datetime(2024,1,2,0,0,0,'TimeZone','UTC');
+sampletime=1;
+
 % https://www.mathworks.com/help/releases/R2024b/aerotbx/ug/satelliteScenario-key-concepts.html#mw_e08739b4-c5da-4983-898b-56e18cc71f87
+
+% 创建图窗
 sc = satelliteScenario();
-groundStation(sc,Name='WHU',Latitude=30.5288888,Longitude=114.3530555,Altitude=56,MinElevationAngle=60);
+
+% 创建地面站
+gs=groundStation(sc,Name='WHU',Latitude=latitude,Longitude=longitude,Altitude=altitude,MinElevationAngle=60);
+
+% 创建和读取卫星，渲染轨道
 % satellite(sc,[pwd,'\gp_single.tle'],OrbitPropagator="sgp4")
-satellite(sc,[pwd,'\gp.tle'],OrbitPropagator="sgp4");
-play(sc);
+sat=satellite(sc,[pwd,'\gp.tle'],OrbitPropagator="sgp4");
+
+% 设置卫星的可见性
+ac=access(gs,sat);
+intvls = accessIntervals(ac);
+
+% % 创建Viewer
+% v=satelliteScenarioViewer(sc,Name='Starlink Scenario Viewer',Basemap='Streets',Dimension='3D');
+% campos(v,latitude,longitude);
+% play(sc);
